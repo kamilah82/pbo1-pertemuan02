05d3f5e34f208d567db13292bd06caf8245407df
kamilah82 <kamilah.uniska@gmail.com>
2020-04-21 14:12:55 +0700

    memanggil

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa =======================");
+  System.out.println("NPM  : 19630881");
+  System.out.println("Nama : Kamilah");
+  System.out.println("=========================================");
+ }
+}
